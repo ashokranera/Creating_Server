f6c59ba8f2c55d8ca62af6688beec08716317251
ashokranera <ashokranera@gmail.com>
2021-08-12 17:05:02 +0530

    Serving JSON Data

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var http = require('http')
 
 var server = http.createServer((req , res) => {
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('hey node');
+    res.writeHead(200,{'Content-Type':'application/json'});
+ 
+    var myObj = {
+        name:'ashok',
+        job : 'nodejs',
+        age : 22
+    };
+
+    res.end(JSON.stringify(myObj))
 })
 
 server.listen(3000,'127.0.0.1');
